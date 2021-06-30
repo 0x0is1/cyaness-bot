8fde1cff620db63582330226a8ce20e4776d17ef
0x0is1 <0x0is1@protonmail.com>
2021-06-30 10:38:17 +0530

    [*] Upgraded lib for upcoming changes

--- cyanesslib/cyanesslib.py
+++ cyanesslib/cyanesslib.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as scraper
-import random
 URL='https://explosm.net/comics/'
 
-def get_image_url():
-  response=requests.get(url=URL+str(random.randint(39, 5010)))
+def get_image_url(comic_index):
+  response=requests.get(url=URL+str(comic_index))
   if 'Could not find comic' in str(response.content):
-    get_image_url()
-  
+    get_image_url(comic_index+1)
   else:
     soup = scraper(response.content, 'html.parser')
     image_url = soup.find('img', {'id': 'main-comic'}).get('src')
